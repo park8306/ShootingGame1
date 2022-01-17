47358cc9054e7461d3135b08fbef99fc849cc1b3
park8306 <kosaf0804@gmail.com>
2022-01-17 19:00:53 +0900

    플레이어 방향키로 움직임 추가 (현재 포지션에 다음 포지션을 더해서 움직임), 경계영역 추가 (4개의 콜라이더를 이용하여 경계영역 생성, 바디타입을 static으로 설정), 플레이어에 box콜라이더 추가

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,18 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
+    public float speed;
     void Update()
     {
-        
+        float h = Input.GetAxisRaw("Horizontal"); // -1, 0, 1 딱 3가지 값만 들어옴
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 curPos = transform.position; // 현재 플레이어의 위치
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime; // 다음에 갈 위치 * 속도
+
+        transform.position = curPos + nextPos;
     }
 }
