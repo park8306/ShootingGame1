fa8ce0fbd23031c7f57e73c341fa1ae5204e9bf2
park8306 <kosaf0804@gmail.com>
2022-01-20 20:18:10 +0900

    GameManager 스크립트 생성!, 게임메니저스크립트를 통해 적생성!

---
+++ Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] enemyObjs;
+    public Transform[] spawnPoints;
+
+    public float maxSpawnDelay;
+    public float curSpawnDelay;
+
+    private void Update()
+    {
+        curSpawnDelay += Time.deltaTime;
+        if(curSpawnDelay > maxSpawnDelay)
+        {
+            SpawnEnemy();
+            maxSpawnDelay = Random.Range(0.5f, 3f);
+            curSpawnDelay = 0;
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        int ranEnemy = Random.Range(0, 3); // 0~2 랜덤
+        int ranPoint = Random.Range(0, 5); // 0~4 랜덤
+        Instantiate(enemyObjs[ranEnemy], spawnPoints[ranPoint].position, spawnPoints[ranPoint].rotation);
+    }
+}
