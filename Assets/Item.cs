1644cefb653f7bef43b1afaa31a8d52290c50e70
park8306 <kosaf0804@gmail.com>
2022-01-22 13:55:31 +0900

    아이템(파워, 폭탄, 코인) 추가 및 애니메이션 추가

---
+++ Assets/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string type;
+    Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.down * 3;
+    }
+}
