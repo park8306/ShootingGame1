96022fe7eca20a563e2ab255475c801d3cbfe941
park8306 <kosaf0804@gmail.com>
2022-01-22 09:18:04 +0900

    적을 양 옆에서도 생성하고 적이 플에이어 방향으로 총알을 발사하도록 구현!

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,17 +4,62 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public string enemyName;
     public float speed;
     public int health;
     public Sprite[] sprites;
 
     SpriteRenderer spriteRenderer;
     Rigidbody2D rigid;
+
+    public GameObject EnemyBulletA;
+    public GameObject EnemyBulletB;
+    public GameObject player;
+
+    public float maxShotDelay;
+    public float curShotDelay;
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>(); // 스프라이트 렌더러를 통해 피격효과 구현
-        rigid = GetComponent<Rigidbody2D>();
-        rigid.velocity = Vector2.down * speed;
+    }
+
+    private void Update()
+    {
+        Fire();
+        Reload();
+    }
+
+    private void Reload()
+    {
+        curShotDelay += Time.deltaTime;
+    }
+
+    private void Fire()
+    {
+        if (curShotDelay < maxShotDelay)
+            return;
+
+        if(enemyName == "S")
+        {
+            GameObject bullet = Instantiate(EnemyBulletA, transform.position, transform.rotation); // 총알 생성하는 코드
+            Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>(); // 총알의 rigidbody2D 값을 가져옴
+
+            Vector3 dirVec = player.transform.position - transform.position; // 플레이어 방향을 구함
+            rigid.AddForce(dirVec.normalized * 10, ForceMode2D.Impulse);   // addForce를 통해 총알에 힘을 부여
+        }
+        else if(enemyName == "L")
+        {
+            GameObject bulletR = Instantiate(EnemyBulletB, transform.position + Vector3.right * 0.3f, transform.rotation); // 총알 생성하는 코드
+            GameObject bulletL = Instantiate(EnemyBulletB, transform.position + Vector3.left * 0.3f, transform.rotation); // 총알 생성하는 코드
+            Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>(); // 총알의 rigidbody2D 값을 가져옴
+            Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>(); // 총알의 rigidbody2D 값을 가져옴
+            Vector3 dirVecR = player.transform.position - (transform.position + Vector3.right * 0.3f);
+            Vector3 dirVecL = player.transform.position - (transform.position + Vector3.left * 0.3f);
+            rigidR.AddForce(dirVecR.normalized * 10, ForceMode2D.Impulse);   // addForce를 통해 총알에 힘을 부여
+            rigidL.AddForce(dirVecL.normalized * 10, ForceMode2D.Impulse);   // addForce를 통해 총알에 힘을 부여
+        }
+
+        curShotDelay = 0;
     }
 
     void OnHit(int dmg)
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,27 +6,45 @@ using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject[] enemyObjs;
-    public Transform[] spawnPoints;
+    public GameObject[] enemyObjs;  // 적 프리펩 저장
+    public Transform[] spawnPoints; // 생성할 위치 저장
 
-    public float maxSpawnDelay;
+    public float maxSpawnDelay; // 생성 딜레이
     public float curSpawnDelay;
 
+    public GameObject player; // 게임 메니저가 플레이어를 들고 있어야 함
     private void Update()
     {
         curSpawnDelay += Time.deltaTime;
         if(curSpawnDelay > maxSpawnDelay)
         {
-            SpawnEnemy();
-            maxSpawnDelay = Random.Range(0.5f, 3f);
+            SpawnEnemy();   // 적 소환
+            maxSpawnDelay = Random.Range(0.5f, 3f); // 생성 딜레이 값을 랜덤으로 조정
             curSpawnDelay = 0;
         }
     }
 
     private void SpawnEnemy()
     {
-        int ranEnemy = Random.Range(0, 3); // 0~2 랜덤
-        int ranPoint = Random.Range(0, 5); // 0~4 랜덤
-        Instantiate(enemyObjs[ranEnemy], spawnPoints[ranPoint].position, spawnPoints[ranPoint].rotation);
+        int ranEnemy = Random.Range(0, 3); // 0~2 랜덤한 적 생성
+        int ranPoint = Random.Range(0, 9); // 0~4 랜덤한 위치에서 생성
+        GameObject enemy = Instantiate(enemyObjs[ranEnemy], spawnPoints[ranPoint].position, spawnPoints[ranPoint].rotation);
+        Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
+        Enemy enemyLogic = enemy.GetComponent<Enemy>();
+        enemyLogic.player = player; // 적에게 플레이어 정보를 넘겨 줌 (플레이어의 위치로 발사하기위해 필요하기 때문)
+        if(ranPoint == 5 || ranPoint == 6)
+        {
+            enemy.transform.Rotate(Vector3.back * 90);
+            rigid.velocity = new Vector2(enemyLogic.speed * (-1), -1);
+        }
+        else if (ranPoint == 7 || ranPoint == 8)
+        {
+            enemy.transform.Rotate(Vector3.forward * 90);
+            rigid.velocity = new Vector2(enemyLogic.speed, -1);
+        }
+        else
+        {
+            rigid.velocity = new Vector2(0, enemyLogic.speed * (-1));
+        }
     }
 }
