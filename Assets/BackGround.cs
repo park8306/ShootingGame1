03e186914df43033ef0e543d7c90bf861c792c52
park8306 <kosaf0804@gmail.com>
2022-01-29 22:51:13 +0900

    움직이는 배경 추가

---
+++ Assets/BackGround.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    public int speed;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos;
+        pos = transform.position;
+        if(pos.y <= -10)
+        {
+            pos.y = 10 ;
+            transform.position = pos;
+        }
+        else
+        {
+            pos.y = pos.y - Time.deltaTime * speed;
+            transform.position = pos;
+        }
+    }
+}
